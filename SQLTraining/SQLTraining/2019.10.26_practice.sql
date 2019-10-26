a266c95e8324dfd064b865087d485ab7ed35096c
Admin <Admin@RADCHENKO-LT>
2019-10-26 23:52:53 +0300

    15 tasks were added.

--- SQLTraining/SQLTraining/2019.10.26_practice.sql
+++ SQLTraining/SQLTraining/2019.10.26_practice.sql
@@ -142,3 +142,47 @@ WHERE [dbo].PlanetsWithRings.HavingRings = 0
 --TO SHOW
 SELECT *
 FROM PlanetsWithRings
+
+/*Задание_11*/
+-- Найдите номер модели, скорость и размер жесткого диска для всех ПК стоимостью менее 500 дол. Вывести: model, speed и hd:
+
+SELECT model, speed, hd
+FROM PC
+WHERE price < 500
+
+/*Задание_12*/
+-- Найдите производителей принтеров. Вывести: maker:
+
+SELECT DISTINCT maker
+FROM Product
+WHERE type LIKE 'Printer'
+
+/*Задание_13*/
+-- Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов, цена которых превышает 1000 дол.:
+
+SELECT model, ram, screen
+FROM Laptop
+WHERE price > 1000
+
+/*Задание_14*/
+-- Найдите все записи таблицы Printer для цветных принтеров.:
+
+SELECT * 
+FROM Printer
+WHERE color = 'y'
+
+/*Задание_15*/
+-- Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол.:
+
+SELECT model, speed, hd 
+FROM PC 
+WHERE cd IN ('12x', '24x') 
+AND price < 600
+
+-- OR
+
+SELECT model, speed, hd 
+FROM PC 
+WHERE price < 600
+AND cd = '12x' 
+OR  cd = '24x' 
